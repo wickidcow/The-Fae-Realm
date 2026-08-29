1eab47161d69447eba13c34d11292c9b1c61cdb6
wickidcow <jaywago@gmail.com>
2026-08-28 22:12:30 -0400

    chore(worldgen): bump generator schema for ecology

--- src/paper/java/com/wickidcow/aetherlegacy/paper/world/FaeGeneratorVersion.java
+++ src/paper/java/com/wickidcow/aetherlegacy/paper/world/FaeGeneratorVersion.java
@@ -9,7 +9,7 @@ package com.wickidcow.aetherlegacy.paper.world;
  */
 public final class FaeGeneratorVersion {
 
-    public static final int CURRENT = 7;
+    public static final int CURRENT = 8;
 
     private FaeGeneratorVersion() {
     }
